7b4142343a36794a2641b6de2566957b78da7120
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-05-31 09:28:29 -0700

    homework 5/31

--- js/chunk.js
+++ js/chunk.js
@@ -1,6 +1,14 @@
 function chunkIt(arr, length) {
     // We're burglars!
-
+    const chonks = []
+    var i = 0;
     
-
+    while (i < arr.length){
+      chonks.push(arr.slice(i, i + length));
+      i += length
+    }
+    return chonks;
   }
+
+  console.log(chunkIt(["potatoes","molasses","greg","wirt"], 2));
+  
